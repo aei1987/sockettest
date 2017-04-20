3b542c208ecd3b69208a67966cf15374c3418964
aei1987 <296963093@qq.com>
2017-04-20 15:41:19 +0800

    create branch more
    add entity note

--- src/com/lyf/entity/User.java
+++ src/com/lyf/entity/User.java
@@ -2,6 +2,9 @@ package com.lyf.entity;
 
 import java.io.Serializable;
 
+/*
+ *用户实体类
+ */
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 3123389002590039929L;
